8a56d85841d1508a3b6a4a5e8ccfe1d6df11860a
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2021-12-29 21:40:10 +0600

    @responsive and @screens

---
+++ 10. tailwindcss@responsive@screen/tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    purge: [],
+    darkMode: "media", // or 'media' or 'class'
+    theme: {
+    screens: {
+        'xd': '320px',
+        // => @media (min-width: 320px) { ... }
+
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+    },
+
+    extend: {
+        screens: {
+            'ls': '991px',
+            // => @media (min-width: 991px) { ... }
+        },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
